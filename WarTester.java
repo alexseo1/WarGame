98f67a1e4dc128cbd56f682c5685fdf0320e9eec
Alex Seo <121053317+alexseo1@users.noreply.github.com>
2023-03-24 11:18:52 -0400

    Create WarTester.java

---
+++ WarTester.java
@@ -0,0 +1,14 @@
+public class WarTester {
+
+    //this class must remain unchanged
+    //your code must work with this test class
+ 
+    public static void main(){
+        
+       Game g = new Game();
+       g.play();
+        
+       
+    }
+
+}
